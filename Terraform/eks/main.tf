b015169ad1fd57c6a288b20c167060a19c623895
Mahesh Kumar <jadhavmaheshkumar0807@gmail.com>
2026-01-23 11:06:15 +0530

    Update main.tf

--- Terraform/eks/main.tf
+++ Terraform/eks/main.tf
@@ -171,8 +171,7 @@ resource "aws_iam_role_policy_attachment" "node_policy" {
     for_each = toset([
         "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy",
         "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy",
-        "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly",
-        "arn:aws:iam::aws:policy/AmazonEBSCSIDriverPolicy"
+        "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
     ])
     policy_arn = each.value
     role = aws_iam_role.eks-node.name
