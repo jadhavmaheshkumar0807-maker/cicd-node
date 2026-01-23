0594b4363b7a374eb8f471c9d4000d240b96e6b2
Mahesh Kumar <jadhavmaheshkumar0807@gmail.com>
2026-01-23 11:17:35 +0530

    Remove unused variables and add outputs for EKS

--- Terraform/eks/output.tf
+++ Terraform/eks/output.tf
@@ -1,62 +1,24 @@
-variable "vpc_cidr_block" {
-    description = "CIDR block for VPC"
-    default = "11.91.0.0/16"
-    type = string
+output "vpc_id" {
+    value = aws_vpc.eks.id
+    description = "VPC iD"
 }
 
-variable "cluster_name" {
-    description = "Name of the VPC for EKS"
-    default = "ammu"  
-    type = string
+output "private_subnet_id" {
+    description = "Private Subnet id"
+    value = aws_subnet.private[*].id  
 }
 
-variable "private_subnet_cidr" {
-    description = "CIDR blocks for private subnet"
-    default = [
-      "11.91.6.0/24",
-      "11.91.7.0/24"
-      ]
-    type = list(string)
+output "public_subnet_id" {
+    description = "Public Subnet ID"
+    value = aws_subnet.public[*].id  
 }
 
-variable "public_subnet_cidr" {
-    description = "CIDR blocks for private subnet"
-    default = [
-      "11.91.8.0/24",
-      "11.91.9.0/24"
-      ]
-    type = list(string)
+output "cluster_endpoint" {
+    description = "EKS cluster endpoint"
+    value = aws_eks_cluster.eks-cluster.endpoint  
 }
 
-variable "availability_zone" {
-    description = "availability zone for the subnet"
-    type = list(string)  
-    validation {
-      condition = length(var.availability_zone) >= length(var.public_subnet_cidr)
-      error_message = "availability_zone list must match subnet count"
-    }
-    default = [ "us-east-1a", "us-east-1b" ]
-}
-
-variable "node_groups" {
-    description = "EKS nodegroup configuration"
-    type = map(object({
-      instance_types = list(string)
-      scaling_config = object({
-        desired_size = number
-        max_size = number
-        min_size = number
-      })
-    }))
-
-    default = {
-      main = {
-        instance_types = [ "c7i-flex.large" ]
-        scaling_config = {
-            desired_size = 4
-            max_size = 6
-            min_size = 2
-        }
-      }
-    }
+output "cluster_name" {
+    description = "EKS Cluster Name"
+    value = aws_eks_cluster.eks-cluster.name
 }
